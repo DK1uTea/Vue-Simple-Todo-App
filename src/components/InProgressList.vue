5462bd385129957578e3891885085105d8784799
Do Quang Tuan <quangtuan9102003@gmail.com>
2025-09-16 11:10:56 +0700

    Refactor Todo components to separate completed and in-progress tasks

---
+++ src/components/InProgressList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Todo } from "@/stores/TodosStore";
+import TodoItem from "./TodoItem.vue";
+
+interface InProgressListProps {
+  data: Todo[];
+}
+
+const props = defineProps<InProgressListProps>();
+</script>
+
+<template>
+  <div class="w-full flex flex-col items-center gap-2">
+    <span class="font-bold text-start w-full">In Progress</span>
+    <div class="w-full flex flex-col items-center gap-3 max-h-[17rem] overflow-y-auto">
+      <TodoItem v-for="todo in props.data" :key="String(todo.id)" :data="todo" />
+    </div>
+  </div>
+</template>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,13 +3,23 @@
 import { useTodosStore, type Todo } from "@/stores/TodosStore";
 import TodoItem from "./TodoItem.vue";
 import { storeToRefs } from "pinia";
+import CompletedList from "./CompletedList.vue";
+import InProgressList from "./InProgressList.vue";
+
+interface TodoListProps {
+  inProgressData: Todo[];
+  completedData: Todo[];
+}
+
+const props = defineProps<TodoListProps>();
 
 const todosStore = useTodosStore();
 const {todoList} = storeToRefs(todosStore);
 </script>
 
 <template>
-  <div class="w-full flex flex-col gap-3 items-center justify-center">
-    <TodoItem v-for="todo in todoList" :key="String(todo.id)" :data="todo" />
+  <div class="w-full flex flex-col gap-5 items-center justify-center">
+    <InProgressList v-if="props.inProgressData.length > 0" :data="props.inProgressData" />
+    <CompletedList v-if="props.completedData.length > 0" :data="props.completedData" />
   </div>
 </template>
